3a7975b375c6b577a3cc596670d88643c05483c4
bonex-do-binex <pedroven11@outlook.pt>
2026-02-26 10:11:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let clicks = 0;
+let clicks = localStorage.getItem("clicks");
+
+if (clicks === null) {
+    clicks = 0;
+} else {
+    clicks = parseInt(clicks);
+}
 
 const counter = document.getElementById("clickCounter");
 const button = document.getElementById("clickButton");
 
-button.addEventListener("click", function () {
+counter.textContent = "Clicks: " + clicks;
+
+button.addEventListener("click", function() {
     clicks++;
     counter.textContent = "Clicks: " + clicks;
+    localStorage.setItem("clicks", clicks);
 });
